231b34bedebd0c8c6b7c474f66db96f60fe012b2
gitaepark97 <parkkitae7@gmail.com>
2022-05-10 22:11:17 +0900

    Move Zeros

---
+++ array/moveZeros.ts
@@ -0,0 +1,14 @@
+export function moveZeros(nums: number[]) {
+  let wIdx: number = 0
+  nums.map(value => {
+    if (value !== 0) {
+      ;[nums[wIdx], value] = [value, nums[wIdx]]
+      wIdx++
+    }
+  })
+  for (const value of nums) {
+    value > wIdx && (nums[value] = 0)
+  }
+
+  return nums
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 import { binarySearch } from './array/binarySearch'
+import { moveZeros } from './array/moveZeros'
 
-console.log(binarySearch([1, 3, 5, 7, 8, 9], 5))
+// console.log(binarySearch([1, 3, 5, 7, 8, 9], 5))
+console.log(moveZeros([4, 0, 4, 7, 0, 0, 6, 5]))
